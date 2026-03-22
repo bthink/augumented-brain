bac20d762c605ef90e553690a58de8b016fcf757
bthink <bartoszfink@gmail.com>
2026-03-22 19:30:34 +0100

    chore: removes some logging

--- main.py
+++ main.py
@@ -12,13 +12,6 @@ import argparse
 import logging
 import sys
 
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
-    datefmt="%H:%M:%S",
-)
-logger = logging.getLogger(__name__)
-
 
 def run_interactive():
     """Interaktywny chat z orchestratorem."""
